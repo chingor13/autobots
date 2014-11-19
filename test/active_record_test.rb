28b8315a134e6f688a02c7ab9151005b99c89f92
Jeff Ching <jching@avvo.com>
2014-11-19 14:55:11 -0800

    adding active record test

--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -1,4 +1,31 @@
 require 'test_helper'
 
+class ProjectSerializer < ActiveModel::Serializer
+  attributes :id, :name
+  has_many :issues
+end
+
+class IssueSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body
+end
+
+class ProjectAssembler < Autobots::Assembler
+  self.serializer = ProjectSerializer
+end
+
 class ActiveRecordTest < ActiveSupport::TestCase
+  fixtures :all
+
+  def test_basic
+    assembler = ProjectAssembler.new(Project.all)
+    data = assembler.roll_out
+
+    assert data.is_a?(Array)
+    data.each do |item|
+      assert item.is_a?(Hash)
+    end
+
+    pp data
+  end
+
 end
