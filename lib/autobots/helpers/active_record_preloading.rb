f30e396922450298ce4bbfa44ef32d0cab7adc2c
Jeff Ching <ching.jeff@gmail.com>
2014-11-19 23:56:45 -0800

    put dual method for activerecord preloading back. 4.0 still had the 3.2 style preloader

--- lib/autobots/helpers/active_record_preloading.rb
+++ lib/autobots/helpers/active_record_preloading.rb
@@ -3,6 +3,9 @@ module Autobots
     module ActiveRecordPreloading
 
       def transform(objects)
+        ActiveRecord::Associations::Preloader.new(objects, preloads).run
+        objects
+      rescue ArgumentError
         ActiveRecord::Associations::Preloader.new.preload(objects, preloads)
         objects
       end
