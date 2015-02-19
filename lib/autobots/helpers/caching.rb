99d0f012df420a8afe2c7c7adc97330ec2a4dfe5
Greg Orlov <greg@avvo.com>
2015-02-19 10:26:10 -0800

    adding an option to force reload the cache

--- lib/autobots/helpers/caching.rb
+++ lib/autobots/helpers/caching.rb
@@ -26,6 +26,12 @@ module Autobots
             acc
           end
 
+          if options[:force_reload]
+            identifiers.each do |identifier|
+              cache.delete identifier
+            end
+          end
+
           # misses: { key => obj }
           @data = BulkCacheFetcher.new(cache).fetch(identifiers) do |misses|
             roll_out(misses.values)
